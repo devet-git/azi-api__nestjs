c522dcd8c341e975a7367eab966773c449ea1b2c
Thang Bui <devet.279@gmail.com>
2024-09-27 13:30:17 +0700

    update .env

--- src/core/jwt.strategy.ts
+++ src/core/jwt.strategy.ts
@@ -1,21 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserDocument } from 'src/modules/user/user.schema';
 import { UserService } from 'src/modules/user/user.service';
-import { Strategy, ExtractJwt } from 'passport-jwt';
-import { User, UserDocument } from 'src/modules/user/user.schema';
-import { UserDto } from 'src/modules/user/dto/user.dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly userService: UserService) {
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'abcutyyxx__sass123adf',
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
   async validate(payload: any): Promise<UserDocument> {
     const user = await this.userService.getUserByUsername(payload.username);
+
     return user;
   }
 }
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,15 +4,21 @@ import { JwtStrategy } from 'src/core/jwt.strategy';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: 'abcutyyxx__sass123adf',
-      signOptions: {
-        expiresIn: '10m',
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
+        },
+      }),
+      inject: [ConfigService],
     }),
+    ConfigModule,
     UserModule,
   ],
   controllers: [AuthController],
